afd112b5f868c90f0b161e28c2a34d812a7b4686
Fatalis <connman2400@gmail.com>
2022-11-13 15:08:07 -0600

    just getting started, questions made

---
+++ index.js
@@ -0,0 +1,37 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    message: 'Enter Project Title',
+    name: 'title',
+  },{
+    type: 'input',
+    message: 'enter project description',
+    name: 'description',
+  },{
+    type: 'input',
+    message: 'Enter project Installation guide',
+    name: 'Install',
+  },{
+    type: 'input',
+    message: 'Enter project usage',
+    name: 'usage',
+  },{
+    type: 'input',
+    message: 'Enter Github username',
+    name: 'username',
+  },{
+    type: 'input',
+    message: 'Enter email adress',
+    name: 'email',
+  }];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
