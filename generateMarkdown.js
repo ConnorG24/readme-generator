2de7425aaceb805d09e0224876fda4287bd6b736
Fatalis <connman2400@gmail.com>
2022-11-15 21:56:39 -0600

    commit

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }else{
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'GPL') {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/licenses/by-nd/4.0` 
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license){
+    return `## Licenses
+    ${license}`
+  }else{
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## README Section Links
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+   
+  
+  
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.licenses)}
+  ## Contributions
+  ${data.contributions}
+  ## Tests
+  ${data.tests}
+  ## Questions  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
+  ## Credits
+  ${data.credits}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genMarkDown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [{
     type: 'input',
@@ -19,19 +21,47 @@ const questions = [{
     name: 'usage',
   },{
     type: 'input',
+    message: 'Enter project contributions',
+    name: 'contributions',
+  },{
+    type: 'input',
+    message: 'Enter test instructions if any',
+    name: 'tests',
+  },{
+    type: 'input',
     message: 'Enter Github username',
-    name: 'username',
+    name: 'github',
   },{
     type: 'input',
     message: 'Enter email adress',
     name: 'email',
+  },{
+    type: 'input',
+    message: 'Enter project credits',
+    name: 'credits',
   }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README!')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   return inquirer.prompt(questions);
+    
+}
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return genMarkDown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile('README.md',readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
